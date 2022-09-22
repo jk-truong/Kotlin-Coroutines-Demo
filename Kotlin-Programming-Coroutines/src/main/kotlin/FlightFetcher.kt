54ab5f6f5f5f86eced066a33cbac1ec976fd64a4
jk-truong <41808114+jk-truong@users.noreply.github.com>
2022-09-22 16:53:07 -0700

    complete ch. 20 Coroutines

--- Kotlin-Programming-Coroutines/src/main/kotlin/FlightFetcher.kt
+++ Kotlin-Programming-Coroutines/src/main/kotlin/FlightFetcher.kt
@@ -1,13 +1,50 @@
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
 import java.net.URL
 
 private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
+private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
 
 fun main() {
-    println("Started")
-    val flight = fetchFlight()
-    println(flight)
-    println("Finished")
+    // runBlocking blocks its thread until coroutine execution is complete
+    runBlocking {
+        println("Started")
+        // launch normal coroutine
+        launch {
+            val flight = fetchFlight("Jo mama")
+            println(flight)
+        }
+        println("Finished")
+    }
 }
 
-fun fetchFlight(): String = URL(FLIGHT_ENDPOINT).readText()
+// Another coroutine that takes in a different context. It is a suspending function, which must be called inside another
+// suspending fun or coroutine builder. coroutineScope creates a new coroutine scope, but inherits the dispatcher so
+// the coroutine will stop when its parent stops.
+suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
+    val client = HttpClient(CIO)
+
+    // Async allows parallel
+    val flightResponse = async {
+        println("Started fetching flight info")
+        client.get<String>(FLIGHT_ENDPOINT).also {
+            println("Finished fetching flight info")
+        }
+    }
+    val loyaltyResponse = async {
+        println("Started fetching loyalty info")
+        client.get<String>(LOYALTY_ENDPOINT).also {
+            println("Finished fetching loyalty info")
+        }
+    }
+
+    delay(500)
+    FlightStatus.parse(
+        passengerName = passengerName,
+        flightResponse = flightResponse.await(),
+        loyaltyResponse = loyaltyResponse.await()
+    )
+}
